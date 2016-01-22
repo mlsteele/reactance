d9b5379ae8f547908a044d153fbdbf37a2bd4bbf
Miles Steele <miles@milessteele.com>
2016-01-22 15:39:15 -0500

    expose store_reset

--- scripts/store-reset.js
+++ scripts/store-reset.js
@@ -3,9 +3,11 @@ module.exports = store_reset
 function store_reset(version) {
     var stored = store.get('STORE_DB_VERSION')
     if (stored === version) {
-        return
+	return
     } else {
-        store.clear()
-        store.set('STORE_DB_VERSION', version)
+	store.clear()
+	store.set('STORE_DB_VERSION', version)
     }
 }
+
+window.store_reset = store_reset;
